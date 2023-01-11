53a3c6769ad85e0302383c9a6a85131e134d38ec
nicolehwang@google.com <nicolehwang@google.com>
2023-01-11 16:32:15 +0900

    fix errors

--- app/src/main/java/com/example/workmanagersample/Constants.kt
+++ app/src/main/java/com/example/workmanagersample/Constants.kt
@@ -1,4 +1,14 @@
+@file:JvmName("Constants")
 package com.example.workmanagersample
 
-class Constants {
-}
+// Name of Notification Channel for verbose notifications of background work
+@JvmField
+val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
+    "Verbose WorkManager Notifications"
+const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION =
+    "Shows notifications whenever work starts"
+@JvmField
+val NOTIFICATION_TITLE: CharSequence = "WorkRequest Starting"
+const val CHANNEL_ID = "VERBOSE_NOTIFICATION"
+const val NOTIFICATION_ID = 1
+
--- app/src/main/java/com/example/workmanagersample/workers/WorkerUtils.kt
+++ app/src/main/java/com/example/workmanagersample/workers/WorkerUtils.kt
@@ -1,3 +1,5 @@
+@file:JvmName("WorkerUtils")
+
 package com.example.workmanagersample.workers
 
 import android.app.NotificationChannel
@@ -6,48 +8,46 @@ import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.work.ForegroundInfo
-import com.example.workmanagersample.R
+import com.example.workmanagersample.*
 
-class WorkerUtils {
 
-    /**
-     * Create a Notification that is shown as a heads-up notification if possible.
-     *
-     * For this codelab, this is used to show a notification so that you know when different steps
-     * of the background work chain are starting
-     *
-     * @param message Message shown on the notification
-     * @param context Context needed to create Toast
-     */
-    fun makeStatusNotification(message: String, context: Context) : ForegroundInfo {
+/**
+ * Create a Notification that is shown as a heads-up notification if possible.
+ *
+ * For this codelab, this is used to show a notification so that you know when different steps
+ * of the background work chain are starting
+ *
+ * @param message Message shown on the notification
+ * @param context Context needed to create Toast
+ */
+fun makeStatusNotification(message: String, context: Context): ForegroundInfo {
 
-        // Make a channel if necessary
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
-            val name = VERBOSE_NOTIFICATION_CHANNEL_NAME
-            val description = VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(CHANNEL_ID, name, importance)
-            channel.description = description
+    // Make a channel if necessary
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        val name = VERBOSE_NOTIFICATION_CHANNEL_NAME
+        val description = VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(CHANNEL_ID, name, importance)
+        channel.description = description
 
-            // Add the channel
-            val notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+        // Add the channel
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
 
-            notificationManager?.createNotificationChannel(channel)
-        }
-
-        // Create the notification
-        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(NOTIFICATION_TITLE)
-            .setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setVibrate(LongArray(0))
-
-        // Show the notification
-
-        return ForegroundInfo(NOTIFICATION_ID, builder.build())
+        notificationManager?.createNotificationChannel(channel)
     }
+
+    // Create the notification
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle(NOTIFICATION_TITLE)
+        .setContentText(message)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setVibrate(LongArray(0))
+
+    // Show the notification
+
+    return ForegroundInfo(NOTIFICATION_ID, builder.build())
 }
